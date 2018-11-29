f96eda03abe3458fb6bbf0294ce5f368d226b64c
Bishal Baaniya <071bct513@pcampus.edu.np>
2018-11-29 18:05:34 +0545

    push

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ let bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.use('/twilio', twilioRouter);
+app.use('/token', twilioRouter);
 // end twilio test
 
 // catch 404 and forward to error handler
--- routes/twilio-route.js
+++ routes/twilio-route.js
@@ -8,8 +8,11 @@ router.get('/', function(req, res, next) {
     res.json(token);
 });
 router.post('/', function(req, res, next) {
-    let token = twilioController.generator(req.body.email, req.body.deviceId);
-    res.json(token);
+    let token = twilioController.generator(req.body.identity, req.body.device);
+    res.json({
+        token: token.toJwt(),
+        identity: req.body.identity
+    });
 });
 
 module.exports = router;
